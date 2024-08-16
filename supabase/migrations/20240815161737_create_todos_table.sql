b456f1867c0c3180580b9cc2ad1acc76dc53e4bb
Ryan D'Onofrio <97113725+Ryandonofrio3@users.noreply.github.com>
2024-08-15 19:00:18 -0700

    Initial commit

--- supabase/migrations/20240815161737_create_todos_table.sql
+++
@@ -1,9 +0,0 @@
-
--- Create todos table
-CREATE TABLE todos (
-  id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-  title TEXT NOT NULL,
-  is_complete BOOLEAN DEFAULT FALSE,
-  inserted_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc', now()) NOT NULL
-);
-
